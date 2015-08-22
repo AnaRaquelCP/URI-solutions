06602ffa71007fd3315ac358d7c3eb78e7df1e92
Ana Raquel <anaraquel@frconsultoria.com.br>
2015-08-22 12:01:33 -0300

    Create 1008.cpp

---
+++ 1008.cpp
@@ -0,0 +1,14 @@
+#include<cstdio>
+#include<stdlib.h>
+ 
+int main (){
+ int A,B;
+ float C,X;
+ scanf("%d",&A);
+ scanf("%d",&B);
+ scanf("%f",&C);
+ X=(B*C);
+ printf("NUMBER = %d\n",A);
+ printf("SALARY = U$ %.2f\n",X);
+ system ("PAUSE");  
+}
