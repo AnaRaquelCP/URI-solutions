c1f4b2e6b40950da95384265a3d91b1c544ea832
Ana Raquel <anaraquel@frconsultoria.com.br>
2015-08-22 12:02:21 -0300

    Create 1006.cpp

---
+++ 1006.cpp
@@ -0,0 +1,12 @@
+#include<cstdio>
+#include<stdlib.h>
+ 
+int main (){
+ float A,B,X,C;
+ scanf("%f",&A);
+ scanf("%f",&B);
+ scanf("%f",&C);
+ X=((A*2)+(B*3)+(C*5))/10;
+ printf("MEDIA = %.1f\n",X);
+ system ("PAUSE");  
+}
